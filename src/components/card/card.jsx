cfc64c45251eb9183bf718b102ef87b4be92a79f
ramon1604 <ramoncott@hotmail.com>
2022-10-14 15:39:08 -0400

    Implicit return after complex destructuring

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,22 +2,20 @@
 import './card.css';
 
 // Functional component
-const Card = ({ monster: { id, name, email } }) => {
-    return (
-        <div className={` card-container `}>
-            <img
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                alt={`monster ${name}`}
-            />
-            <h2>
-                {name}
-            </h2>
-            <p>
-                {email}
-            </p>
-        </div>
-    )
-}
+const Card = ({ monster: { id, name, email } }) => (
+    <div className={` card-container `}>
+        <img
+            src={`https://robohash.org/${id}?set=set2&size=180x180`}
+            alt={`monster ${name}`}
+        />
+        <h2>
+            {name}
+        </h2>
+        <p>
+            {email}
+        </p>
+    </div>
+)
 
 /*
 // Class component
